fd160308a337cdfab803a5c5b220df8f6833c719
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-11-20 11:45:24 +0100

    config gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
+import "@popperjs/core";
+import "bootstrap";
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
